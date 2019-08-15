58dd93c4567e82590ccee4042798d13c77f9a247
Jesus Guadiana <jisus130@hotmail.com>
2019-08-14 21:14:04 -0500

    Styled login page and implemented login logic. Missing password encryption

---
+++ server/src/routes.js
@@ -0,0 +1,8 @@
+const AuthenticationController = require('./controllers/AuthenticationController')
+
+const AuthenticationControllerPolicy = require('./polices/AuthenticationControllerPolicy')
+module.exports = (app) => {
+    app.post('/register',
+    AuthenticationControllerPolicy.register,
+    AuthenticationController.register)
+}
